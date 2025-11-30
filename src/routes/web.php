fe914761b30ffb8bfb22919e8f274b9c45f4a9c4
mariko inukai <mariko.inukai1006@gmail.com>
2025-11-30 22:39:33 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index']);
+// Route::post('/contacts/confirm', [TodoController::class, 'confirm']);
+// Route::post('/contacts', [TodoController::class, 'store']);
